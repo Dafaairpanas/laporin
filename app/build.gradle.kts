35c13921d894976df6900c63b802e901e25fa529
Dafaairpanas <dafaoffic@gmail.com>
2025-11-05 09:49:01 +0700

    re- detail activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,12 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
+
+
 
 dependencies {
 
@@ -52,4 +57,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
 }
